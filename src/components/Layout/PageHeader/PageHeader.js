1317468f76402953f6a9d71bd3f0293d596cbe0c
Lucy Hutcheson <lhutcheson@element84.com>
2023-02-02 11:38:34 -0800

    Add custom logo functionality

--- src/components/Layout/PageHeader/PageHeader.js
+++ src/components/Layout/PageHeader/PageHeader.js
@@ -1,22 +1,26 @@
-import React from "react";
-import "./PageHeader.css";
+import React from 'react'
+import './PageHeader.css'
 
-import logoFilmDrop from "../../../assets/logo-filmdrop-white.svg";
-
-import { OpenInNew } from "@mui/icons-material";
+import { OpenInNew } from '@mui/icons-material'
 
 const PageHeader = () => {
+  const DASHBOARD_LINK = process.env.REACT_APP_DASHBOARD_LINK
+  const LOGO = process.env.REACT_APP_LOGO
 
-  const DASHBOARD_LINK = process.env.REACT_APP_DASHBOARD_LINK;
-
-  function onDashboardClick() {
-    window.open(DASHBOARD_LINK, "_blank");
+  function onDashboardClick () {
+    window.open(DASHBOARD_LINK, '_blank')
   }
 
   return (
     <div className="PageHeader">
       <div className="pageHeaderLeft">
-        <img src={logoFilmDrop} alt="logo" className="headerLogoImage"></img>
+        {LOGO
+          ? (
+          <img src={LOGO} alt="logo" className="headerLogoImage"></img>
+            )
+          : (
+          <img src="/logo.png" alt="logo" className="headerLogoImage"></img>
+            )}
       </div>
       {DASHBOARD_LINK && (
         <div className="pageHeaderRight" onClick={() => onDashboardClick()}>
@@ -27,7 +31,7 @@ const PageHeader = () => {
         </div>
       ) }
     </div>
-  );
-};
+  )
+}
 
-export default PageHeader;
+export default PageHeader
