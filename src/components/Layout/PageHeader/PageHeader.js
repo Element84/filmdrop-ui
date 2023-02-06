f2f286befa6c20e2a92a4c29e733c135071452c7
Lucy Hutcheson <lhutcheson@element84.com>
2023-02-06 15:25:52 -0800

    Update logo alt variable name

--- src/components/Layout/PageHeader/PageHeader.js
+++ src/components/Layout/PageHeader/PageHeader.js
@@ -6,7 +6,7 @@ import { OpenInNew } from '@mui/icons-material'
 const PageHeader = () => {
   const DASHBOARD_LINK = process.env.REACT_APP_DASHBOARD_LINK
   const LOGO = process.env.REACT_APP_LOGO
-  const ALT_TEXT = process.env.REACT_APP_LOGO_ALT_VALUE
+  const ALT_TEXT = process.env.REACT_APP_LOGO_ALT
 
   function onDashboardClick () {
     window.open(DASHBOARD_LINK, '_blank')
