fa2f3dbf3c2953977bac704ef79629930a24bded
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2023-03-15 13:55:49 -0700

    Add rescale, colormap, expression tiler parameters

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## v.0.5.0 - TBD
+
+### Changed
+
+- Move mosaic-specific tiler parameters (`mosaic_asset` and `mosaic_color_formula`) into new env variable REACT_APP_MOSAIC_TILER_PARAMS
+
+### Added
+
+- Env variable REACT_APP_MOSAIC_TILER_PARAMS
+- `rescale`, `colormap_name`, and `expression` tiler parameters
+
 ## v0.4.0 - 2023-03-14
 
 ### Changed
--- src/components/Search/envVarSetup.js
+++ src/components/Search/envVarSetup.js
@@ -2,12 +2,54 @@
 export const envTilerURL = process.env.REACT_APP_TILER_URL || ''
 export const envMosaicTilerURL = process.env.REACT_APP_MOSAIC_TILER_URL || ''
 
+// retrieve tiler params from env variables for scene and mosaic
+const getTilerParams = () => {
+  try {
+    return JSON.parse(process.env.REACT_APP_TILER_PARAMS)
+  } catch (e) {
+    console.log(`Error parsing tiler params: ${e.message}`)
+  }
+  return {}
+}
+
+const getMosaicTilerParams = () => {
+  try {
+    return JSON.parse(process.env.REACT_APP_MOSAIC_TILER_PARAMS)
+  } catch (e) {
+    console.log(`Error parsing tiler params: ${e.message}`)
+  }
+  return {}
+}
+
+// construct assets params from env variables for scene and mosaic
+const constructAssetsParam = (collection, tilerParams) => {
+  const assets = tilerParams[collection]?.assets || []
+  if (!assets) {
+    console.log(`Assets not defined for ${collection}`)
+    return [null, '']
+  }
+  // titiler accepts multiple `assets` parameters for compositing
+  // multiple files, so add extra params here if there's more than
+  // one asset specified
+  return [assets[0], `assets=${assets.join('&assets=')}`]
+}
+export const constructMosaicAssetVal = (collection) => {
+  const asset = getMosaicTilerParams()[collection]?.assets || ''
+  if (!asset) {
+    console.log(`Assets not defined for ${collection}`)
+    return null
+  } else {
+    return asset.toString()
+  }
+}
+
 // function to construct the Titiler tile query parameters from
 // REACT_APP_TILER_PARAMS env var
 export const constructTilerParams = (collection) => {
-  const params = []
-
+  // retrieve mosaic tiler parameters from env variable
   const tilerParams = getTilerParams()
+
+  const params = []
 
   const [asset, assetsParam] = constructAssetsParam(collection, tilerParams)
 
@@ -24,16 +66,31 @@ export const constructTilerParams = (collection) => {
     params.push(`asset_bidx=${assetBidx}`)
   }
 
+  const expression = tilerParams[collection]?.expression
+  if (expression) {
+    params.push(`expression=${expression}`)
+  }
+
+  const rescale = tilerParams[collection]?.rescale
+  if (rescale) {
+    params.push(`rescale=${rescale}`)
+  }
+
+  const colormap = tilerParams[collection]?.colormap_name
+  if (colormap) {
+    params.push(`colormap_name=${colormap}`)
+  }
+
   return params.join('&')
 }
 
 export const constructMosaicTilerParams = (collection) => {
-  // retrieve tiler parameters from env variable
-  const tilerParams = getTilerParams()
+  // retrieve mosaic tiler parameters from env variable
+  const tilerParams = getMosaicTilerParams()
 
   const params = []
 
-  const colorFormula = tilerParams[collection]?.mosaic_color_formula
+  const colorFormula = tilerParams[collection]?.color_formula
   if (colorFormula) {
     params.push(`color_formula=${colorFormula}`)
   }
@@ -43,36 +100,15 @@ export const constructMosaicTilerParams = (collection) => {
     params.push(`bidx=${bidx}`)
   }
 
+  const rescale = tilerParams[collection]?.rescale
+  if (rescale) {
+    params.push(`rescale=${rescale}`)
+  }
+
+  const colormap = tilerParams[collection]?.colormap_name
+  if (colormap) {
+    params.push(`colormap_name=${colormap}`)
+  }
+
   return params.join('&')
-}
-
-const getTilerParams = () => {
-  try {
-    return JSON.parse(process.env.REACT_APP_TILER_PARAMS)
-  } catch (e) {
-    console.log(`Error parsing tiler params: ${e.message}`)
-  }
-  return {}
-}
-
-const constructAssetsParam = (collection, tilerParams) => {
-  const assets = tilerParams[collection]?.assets || []
-  if (!assets) {
-    console.log(`Assets not defined for ${collection}`)
-    return [null, '']
-  }
-  // titiler accepts multiple `assets` parameters for compositing
-  // multiple files, so add extra params here if there's more than
-  // one asset specified
-  return [assets[0], `assets=${assets.join('&assets=')}`]
-}
-
-export const constructMosaicAssetVal = (collection) => {
-  const asset = getTilerParams()[collection]?.mosaic_asset || ''
-  if (!asset) {
-    console.log(`Assets not defined for ${collection}`)
-    return null
-  } else {
-    return asset
-  }
 }
