515adffc067c452c884689186cb78c2b65e63c84
Matt Bialas <mbialas@element84.com>
2023-05-04 15:03:51 -0400

    Example config cleanup

--- public/config.example.js
+++ public/config.example.js
@@ -1,10 +1,10 @@
 export const PUBLIC_URL = 'http://example.com/'
-export const VITE_LOGO_ALT = 'Element 84 logo in solid white'
+export const VITE_LOGO_ALT = 'Alt description for my custom logo'
 export const VITE_SHOW_PUBLISH_BTN = false
-export const VITE_DEFAULT_COLLECTION = 'sentinel-2-l2a'
-export const VITE_STAC_API_URL = 'https://earth-search.aws.element84.com/v1'
+export const VITE_DEFAULT_COLLECTION = 'collection-name'
+export const VITE_STAC_API_URL = 'https://api-endpoint.example.com'
 export const VITE_API_MAX_ITEMS = 200
-export const VITE_SCENE_TILER_URL = 'https://titiler.dev.demo.filmdrop.io'
+export const VITE_SCENE_TILER_URL = 'https://titiler.example.com'
 export const VITE_DASHBOARD_BTN_URL = 'https://dashboard.example.com'
 export const VITE_ANALYZE_BTN_URL = 'https://dashboard.example.com'
 export const VITE_SCENE_TILER_PARAMS = {
@@ -31,7 +31,7 @@ export const VITE_SCENE_TILER_PARAMS = {
   }
 }
 export const VITE_MOSAIC_TILER_URL =
-  'https://titiler-mosaic.dev.aws.element84.com/'
+  'https://titiler-mosaic.example.com'
 export const VITE_MOSAIC_TILER_PARAMS = {
   'sentinel-2-l2a': { assets: ['visual'] },
   'landsat-c2-l2': {
@@ -67,7 +67,7 @@ export const VITE_SEARCH_MIN_ZOOM_LEVELS = {
   'cop-dem-glo-90': { medium: 6, high: 8 },
   'sentinel-1-grd': { medium: 4, high: 7 }
 }
-export const VITE_APP_NAME = 'Earth on AWS Viewer'
+export const VITE_APP_NAME = 'App Name'
 export const VITE_CF_TEMPLATE_URL =
-  'https://eoav-lyo-templates.s3.us-west-2.amazonaws.com/eoav.dev.template.json'
+  'https://cf-template.example.com'
 export const VITE_LOGO_URL = './logo.png'
