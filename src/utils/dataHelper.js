f3ab624e06294d396195eec9286d1fbcd8e8e6e5
Brad Andrick <brad.andrick@ursaspace.com>
2023-08-07 14:51:09 -0400

    more refactor of cart popup results and map layer

--- src/utils/dataHelper.js
+++ src/utils/dataHelper.js
@@ -1,6 +1,12 @@
 import { GetCollectionQueryablesService } from '../services/get-queryables-service'
 import { GetCollectionAggregationsService } from '../services/get-aggregations-service'
 import { LoadLocalGridDataService } from '../services/get-local-grid-data-json-service'
+import { store } from '../redux/store'
+import {
+  cartFootprintLayerStyle,
+  addDataToLayer,
+  clearLayer
+} from './mapHelper'
 
 export async function buildCollectionsData(collections) {
   for (const collection of collections.collections) {
@@ -24,4 +30,52 @@ export async function loadLocalGridData() {
   dataFiles.map(async function (d) {
     await LoadLocalGridDataService(d)
   })
+}
+
+export function isSceneInCart(sceneObject) {
+  let itemInCart = false
+  store.getState().mainSlice.cartItems.forEach((cartItem) => {
+    if (cartItem.id === sceneObject.id) {
+      itemInCart = true
+    }
+  })
+  return itemInCart
+}
+
+export function numberOfSelectedInCart(results) {
+  let count = 0
+  results.forEach((result) => {
+    const sceneInCart = isSceneInCart(result)
+    if (sceneInCart) {
+      count++
+    }
+  })
+  return count
+}
+
+export function areAllScenesSelectedInCart(results) {
+  let allInCart = true
+  results.forEach((result) => {
+    const sceneInCart = isSceneInCart(result)
+    if (!sceneInCart) {
+      allInCart = false
+    }
+  })
+  return allInCart
+}
+
+export function setScenesForCartLayer() {
+  if (store.getState().mainSlice.cartItems.length === 0) {
+    clearLayer('cartFootprintsLayer')
+    return
+  }
+  const cartGeojson = {
+    type: 'FeatureCollection',
+    features: store.getState().mainSlice.cartItems
+  }
+  const options = {
+    style: cartFootprintLayerStyle,
+    interactive: false
+  }
+  addDataToLayer(cartGeojson, 'cartFootprintsLayer', options)
 }
