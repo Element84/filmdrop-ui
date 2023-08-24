0a2856493cbea9fe8b360251b18ba5533e4c86fc
Brad Andrick <brad.andrick@ursaspace.com>
2023-08-24 14:56:49 -0400

    add resize observer polyfill for react-tooltip

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,8 +3,10 @@ import { cleanup } from '@testing-library/react'
 import matchers from '@testing-library/jest-dom/matchers'
 import { mainSliceReset } from './redux/slices/mainSlice'
 import { store } from './redux/store'
+import 'resize-observer-polyfill'
 
 window.HTMLCanvasElement.prototype.getContext = () => {}
+global.ResizeObserver = require('resize-observer-polyfill')
 
 expect.extend(matchers)
 
