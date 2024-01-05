9fdf0b10b2f2c6ecb247b190de35603f52a1dfc9
Brad Andrick <bandrick@element84.com>
2024-01-05 11:49:43 -0500

    adding search button

--- src/components/Layout/Content/LeftContent/LeftContent.jsx
+++ src/components/Layout/Content/LeftContent/LeftContent.jsx
@@ -4,14 +4,21 @@ import CollectionDropdown from '../../../CollectionDropdown/CollectionDropdown'
 import DateTimeRangeSelector from '../../../DateTimeRangeSelector/DateTimeRangeSelector'
 import CloudSlider from '../../../CloudSlider/CloudSlider'
 import ViewSelector from '../../../ViewSelector/ViewSelector'
+import { newSearch } from '../../../../utils/searchHelper'
 
 const LeftContent = () => {
+  function processSearchBtn() {
+    newSearch()
+  }
   return (
     <div className="LeftContent">
       <CollectionDropdown></CollectionDropdown>
       <DateTimeRangeSelector></DateTimeRangeSelector>
       <CloudSlider></CloudSlider>
       <ViewSelector></ViewSelector>
+      <button className={`actionButton`} onClick={() => processSearchBtn()}>
+        Search
+      </button>
     </div>
   )
 }
