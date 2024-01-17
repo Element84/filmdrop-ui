10950124adb9d4f65b3e729edb6581fdcc384cfd
Brad Andrick <bandrick@element84.com>
2024-01-17 14:54:20 -0500

    refactor controls into vertical search

--- src/components/Layout/Content/LeftContent/LeftContent.jsx
+++ src/components/Layout/Content/LeftContent/LeftContent.jsx
@@ -1,24 +1,13 @@
 import React from 'react'
 import './LeftContent.css'
-import CollectionDropdown from '../../../CollectionDropdown/CollectionDropdown'
-import DateTimeRangeSelector from '../../../DateTimeRangeSelector/DateTimeRangeSelector'
-import CloudSlider from '../../../CloudSlider/CloudSlider'
-import ViewSelector from '../../../ViewSelector/ViewSelector'
-import { newSearch } from '../../../../utils/searchHelper'
+import Search from '../../../Search/Search'
 
 const LeftContent = () => {
-  function processSearchBtn() {
-    newSearch()
-  }
   return (
     <div className="LeftContent">
-      <CollectionDropdown></CollectionDropdown>
-      <DateTimeRangeSelector></DateTimeRangeSelector>
-      <CloudSlider></CloudSlider>
-      <ViewSelector></ViewSelector>
-      <button className={`actionButton`} onClick={() => processSearchBtn()}>
-        Search
-      </button>
+      <div className="LeftContentHolder">
+        <Search></Search>
+      </div>
     </div>
   )
 }
