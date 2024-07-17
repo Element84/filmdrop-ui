e6d53d3a71ada372bb34ed0846f9b66875eba5ba
Brad Andrick <bandrick@element84.com>
2024-07-17 13:59:39 -0400

    add auth service

---
+++ src/services/post-auth-service.js
@@ -0,0 +1,38 @@
+import { store } from '../redux/store'
+import { setauthTokenExists } from '../redux/slices/mainSlice'
+
+export async function AuthService(username, password) {
+  const AuthServiceURL = store.getState().mainSlice.appConfig.AUTH_URL
+
+  const myHeaders = new Headers()
+  myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+
+  const urlencoded = new URLSearchParams()
+  urlencoded.append('grant_type', 'password')
+  urlencoded.append('username', username)
+  urlencoded.append('password', password)
+
+  const reqParams = {
+    method: 'POST',
+    headers: myHeaders,
+    body: urlencoded
+  }
+
+  await fetch(`${AuthServiceURL}`, reqParams)
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw new Error()
+    })
+    .then((json) => {
+      localStorage.setItem('STAC_Auth_Token', json.access_token)
+      store.dispatch(setauthTokenExists(true))
+    })
+    .catch((error) => {
+      store.dispatch(setauthTokenExists(false))
+      const message = 'Authentication Error'
+      // log full error for diagnosing client side errors if needed
+      console.error(message, error)
+    })
+}
